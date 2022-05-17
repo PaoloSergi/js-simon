41d38c597c9744d15453dfb90a205f6d6b890214
PaoloGit <paolo.sergi.88@gmail.com>
2022-05-17 22:05:52 +0200

    modificato margini

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ for (let i=0; i < numbers.length; i++){
     outputDiv.append(numbers[i]);
 }
 
-setTimeout (checkingPlayerResult, 30000);
+setTimeout (checkingPlayerResult, 2000);
 
 function checkingPlayerResult (){
 
